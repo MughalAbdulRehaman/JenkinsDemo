2164fa4f712547aa82029de57e4acead22b68a63
MughalAbdulRehaman <mughalrehaman@gmail.com>
2026-02-04 11:42:18 +0530

    First

---
+++ JenkinsDemo/src/main/java/com/Jenkins/JenkinsDemo/Controller/JenkinsController.java
@@ -0,0 +1,14 @@
+package com.Jenkins.JenkinsDemo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @RequestMapping("/hello")
+    public String helloJenkins() {
+        return "Hello, Jenkins!";
+    }
+}
